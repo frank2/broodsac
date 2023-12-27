9bf740108d37665766c90f772eabc77b97c0aa93
frank2 <frank2@dc949.org>
2023-12-26 19:19:06 -0800

    add the infector

---
+++ src/infector.cpp
@@ -0,0 +1,67 @@
+#include <cstdint>
+#include <windows.h>
+
+typedef struct FULL_PEB_LDR_DATA
+{
+   ULONG Length;
+   BOOLEAN Initialized;
+   HANDLE SsHandle;
+   LIST_ENTRY InLoadOrderModuleList;
+   LIST_ENTRY InMemoryOrderModuleList;
+   LIST_ENTRY InInitializationOrderModuleList;
+   PVOID EntryInProgress;
+   BOOLEAN ShutdownInProgress;
+   HANDLE ShutdownThreadId;
+} FULL_PEB_LDR_DATA, *PFULL_PEB_LDR_DATA;
+
+typedef struct FULL_LDR_DATA_TABLE_ENTRY
+{
+     LIST_ENTRY InLoadOrderLinks;
+     LIST_ENTRY InMemoryOrderLinks;
+     LIST_ENTRY InInitializationOrderLinks;
+     PVOID DllBase;
+     PVOID EntryPoint;
+     ULONG SizeOfImage;
+     UNICODE_STRING FullDllName;
+     UNICODE_STRING BaseDllName;
+     ULONG Flags;
+     WORD LoadCount;
+     WORD TlsIndex;
+     union
+     {
+          LIST_ENTRY HashLinks;
+          struct
+          {
+               PVOID SectionPointer;
+               ULONG CheckSum;
+          };
+     };
+     union
+     {
+          ULONG TimeDateStamp;
+          PVOID LoadedImports;
+     };
+     PVOID EntryPointActivationContext;
+     PVOID PatchInformation;
+     LIST_ENTRY ForwarderLinks;
+     LIST_ENTRY ServiceTagLinks;
+     LIST_ENTRY StaticLinks;
+} FULL_LDR_DATA_TABLE_ENTRY, *PFULL_LDR_DATA_TABLE_ENTRY;
+
+void infect(void)
+{
+#if defined(BROODSAC32)
+   PPEB peb = reinterpret_case<PPEB>(__readfsdword(0x30));
+#elif defined(BROODSAC64)
+   PPEB peb = reinterpret_cast<PPEB>(__readgsqword(0x60));
+#endif
+
+   PFULL_PEB_LDR_DATA ldr = reinterpret_cast<PFULL_PEB_LDR_DATA>(peb->Ldr);
+   PLIST_ENTRY list_entry = ldr->InLoadOrderModuleList.Flink;
+}
+
+int wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+   infect();
+   return 0;
+}
