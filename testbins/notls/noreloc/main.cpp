bf44c38f77defe8040f90a7ddb33cbfd0431c5b9
frank2 <frank2@dc949.org>
2024-01-03 18:25:49 -0800

    add test binaries to infect

---
+++ testbins/notls/noreloc/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+   std::cout << "* no tls" << std::endl;
+   std::cout << "* no reloc" << std::endl;
+   return 0;
+}
---
+++ testbins/tls/reloc/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include <windows.h>
+
+VOID WINAPI tls_callback(PVOID dllHandle, DWORD reason, PVOID reserved)
+{
+   if (reason == DLL_PROCESS_ATTACH)
+      std::cout << "* tls callback" << std::endl;
+}
+
+#ifdef _M_AMD64
+#pragma comment(linker, "/INCLUDE:_tls_used")
+#pragma comment(linker, "/INCLUDE:p_tls_callback")
+#pragma const_seg(push)
+#pragma const_seg(".CRT$XLAAA")
+EXTERN_C const PIMAGE_TLS_CALLBACK p_tls_callback = tls_callback;
+#pragma const_seg(pop)
+#endif
+#ifdef _M_IX86
+#pragma comment(linker, "/INCLUDE:__tls_used")
+#pragma comment(linker, "/INCLUDE:_p_tls_callback")
+#pragma data_seg(push)
+#pragma data_seg(".CRT$XLAAA")
+EXTERN_C PIMAGE_TLS_CALLBACK p_tls_callback = tls_callback;
+#pragma data_seg(pop)
+
+int main(int argc, char *argv[])
+{
+   std::cout << "* tls" << std::endl;
+   std::cout << "* reloc" << std::endl;
+   return 0;
+}
