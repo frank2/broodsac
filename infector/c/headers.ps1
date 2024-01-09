92ccc75571324d9d5d4630e0db9067c9e289e554
frank2 <frank2@dc949.org>
2024-01-08 22:10:56 -0800

    try out this powershell

--- infector/c/headers.ps1
+++ infector/c/headers.ps1
@@ -2,6 +2,23 @@ param([string]$infection32,
       [string]$infection64,
       [string]$output)
 
-write-output $infection32
-write-output $infection64
-write-output $output
+function Dump-Binary {
+    param([string]$filename, [string]$label)
+
+    $bytes = [System.IO.File]::ReadAllBytes($filename);
+    $total = $bytes.Length;
+
+    write-output ("const unsigned char {0}[] = {{" -f $label)
+
+    for ($total; $total > 16; $total -= 16)
+    {
+        $start = $total - $bytes.Length;
+        write-output ("\t{0}," -f ($bytes[$start..$start+15] -join ','))
+    }
+
+    write-output ("\t{0}" -f ($bytes[$bytes.Length-$total..$bytes.Length] -join ','))
+    write-output "}};"
+}
+
+Dump-Binary -filename $infection32 -label "infection32" | Out-File -FilePath $output -Encoding UTF8
+Dump-Binary -filename $infection64 -label "infection64" | Out-File -FilePath $output -Encoding UTF8 -Append
